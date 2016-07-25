463d4492180d6f90dd9c688dea16d792033be07b
Anas Mourad <anasmouradb2a@gmail.com>
2016-07-24 19:46:37 -0500

    Required Gems'MaterializedCss' added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TutorialsPlatform::Application.config.secret_key_base = '28d68b954c25458454efb86e5f08d568054f5a68ac4f3fd981cb9be973316c31a633e89971bbe78a1b2780ea9d604a347658a52a04b809a4288af5ebd6edde3a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TutorialsPlatform::Application.config.session_store :cookie_store, key: '_tutorialsPlatform_session'
