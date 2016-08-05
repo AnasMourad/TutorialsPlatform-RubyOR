44a0968d4862730d339609c18b0277a8e4613f79
Anas Mourad <anasmouradb2a@gmail.com>
2016-08-04 19:52:49 -0500

    Materialize CSS is correctly added to the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 TutorialsPlatform::Application.routes.draw do
+  get "pages/about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
